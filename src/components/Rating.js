eb9cdaa40c78c8d1cabc02f15ac00e15395d3596
JordanBadcock <77495318+JordanBadcock@users.noreply.github.com>
2023-04-06 21:15:59 -0230

    Initial Commit

---
+++ src/components/Rating.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Card from '../components/Card';
+
+import './Rating.css';
+
+const Rating = () => {
+    return (
+        <Card>
+            <div className='star_img_container'>
+                <img src='' alt=''/>
+            </div>
+            <h2>How did we do?</h2>
+            <p className='text'>
+                Please let us know how we did with your support request. All feedback is 
+                appreciated to help us improve our offering!
+            </p>
+            <div className='rating'>
+                <div className='rating_container'>1</div>
+                <div className='rating_container'>2</div>
+                <div className='rating_container'>3</div>
+                <div className='rating_container'>4</div>
+                <div className='rating_container'>5</div>
+            </div>
+            <button className='submit_btn' >Submit</button>
+        </Card>
+    )
+}
+
+export default Rating;
